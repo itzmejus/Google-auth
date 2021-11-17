30132594c0bcd8c53d4c2cae50c987c30e670d7d
itzmejus <itzjustin3344@gmail.com>
2021-11-17 02:39:37 -0800

    added firebase SDK

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCe8hl_girBrwribigRXrXoeg0dycGYz5E",
+  authDomain: "authentication-eb69e.firebaseapp.com",
+  projectId: "authentication-eb69e",
+  storageBucket: "authentication-eb69e.appspot.com",
+  messagingSenderId: "665873559439",
+  appId: "1:665873559439:web:8b892bbae2b840ed4aa5fa"
+};
+
+
+const app = initializeApp(firebaseConfig);
